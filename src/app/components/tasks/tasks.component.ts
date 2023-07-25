d69ae04558cf3ba613c28a5d737a24f532388f54
Rohan Patel <patelrohan@users.noreply.github.com>
2023-07-25 16:28:16 -0400

    Delete functionality done. Created local db.json

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,4 +15,12 @@ export class TasksComponent {
     // this.tasks = this.taskService.getTasks();
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,19 +2,28 @@ import { Injectable } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  constructor(private httpClinet: HttpClient) {}
 
+  private apiUrl = 'http://localhost:5002/tasks';
   // getTasks(): Task[] {
   //   return TASKS;
   // }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    // return tasks;
+
+    return this.httpClinet.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.httpClinet.delete<Task>(url);
   }
 }
