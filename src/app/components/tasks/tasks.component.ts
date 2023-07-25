67e857525ea754527d92ce49bb642ed8aba79ca3
Rohan Patel <patelrohan@users.noreply.github.com>
2023-07-25 14:54:11 -0400

    Basic tasks loading from local JSON server

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+  ngOnInit(): void {
+    // this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+}
